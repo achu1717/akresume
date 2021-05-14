110422f96bdaf872c1e124d97c30509afc60a1ca
achu1717 <akshaysuresh21@yahoo.com>
2021-05-14 20:59:35 +0530

    homepage work done

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,14 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import {Memory, Home,School,Receipt,Stars,TwoWheeler} from '@material-ui/icons';
 export default function About() {
-    return (
-        <div style={{
-            fontSize:'2rem',
-            display:'flex',
-            justifyContent:'center',
-            marginTop:'2rem',
-        }}>
-            About page
+  return (
+    <div class="maindiv">
+      <h1 className="headings">ABOUT</h1>
+      <div class="parentcdv">
+        <div class="childcdv">
+        <center><Home style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">HOMETOWN</p>
+         
+          <p>
+            Currently i live in Kannur, Kerala. Kannur is well known for a ritual
+            dance called Theyyam. Theyyam consisted of several thousand-year-old
+            traditions, rituals and customs.
+          </p>
         </div>
-    )
+        <div class="childcdv">
+        <center><School style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">EDUCATION</p>
+          <p>Master of Computer Application graduated from Rathnavel Subramaniam college of Arts and Science, Sulur, TamilNadu
+          in 2021.</p>
+        </div>
+        <div class="childcdv">
+        <center><Receipt style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">CERTIFICATIONS</p>
+          <p>Cisco Certified Network Associate, Routing and Switching.</p>
+          <p>Cisco Certified Network Professional, Enterprise</p>
+          <p>The fundamentals of Digital Marketing from Google</p>
+        </div>
+      </div>
+      <div class="parentcdv">
+        <div class="childcdv">
+        <center><Stars style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">SKILLS</p>
+        <p></p>
+        </div>
+        <div class="childcdv">
+        <center><TwoWheeler style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">HOBBIES</p>
+          
+        </div>
+        <div class="childcdv">
+        <center><Memory style={{ fontSize: 190 }}/></center>
+          <p className="headingtech">PROJECTS</p>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,34 @@
 import React from 'react'
-
+import './Home.css';
+import dp from '../images/dp.PNG'
+import rct from '../images/rct.png'
+import ndjs from '../images/ndjs.png'
+import expjs from '../images/expjs.png'
+import mongdb from '../images/mngdb.png'
 export default function Home() {
     return (
-        <div style={{
-            fontSize:'2rem',
-            display:'flex',
-            justifyContent:'center',
-            marginTop:'2rem',
-        }}>
-            Home page
+        <div className="maindiv">
+        <br/><br/>
+        <div className="aboutbx">
+          <center><img  className="dp" src={dp}/></center>
+          
+          <h1 className="head2">AKSHAY SURESH</h1> 
+          <h3 className="head3">Full stack Web Developer</h3> 
+              <p className="contentss">I am a Master of Computer Application graduate.
+Currently working as a freelance frontend developer 
+and is always interested in a challenge.</p>
+</div>
+<div className="tech">
+<h1 className="headingtech">Technologies</h1> 
+<p className="contentss">I use these Technologies for the Web developement</p>
+<center><img  className="tecicon1" src={rct}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img  className="tecicon2" src={ndjs}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img  className="tecicon3" src={expjs}/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img  className="tecicon4" src={mongdb}/></center>
+</div>
+<p className="contentss">Reach out to achuakshay92@gmail.com to connect!</p>
+<h1 className="headings">ABOUT</h1> 
+         
         </div>
     )
 }
